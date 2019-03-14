64bc40d986d23e73f6c197bdc01d1cdcf6c20fb5
Jaime Santana <jaimesantanal@hotmail.com>
2019-03-14 05:15:03 -0500

    template base

---
+++ database/seeds/VehicleTypesSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class VehicleTypesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
